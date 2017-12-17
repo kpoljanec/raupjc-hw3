72f39cbc570de2554293e57b9a37ca28bbac497d
Karlo Poljanec <kp49062@fer.hr>
2017-12-17 21:30:47 +0100

    Adding project

---
+++ Zadatak1/TodoSqlRepository.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.EntityFramework.Exceptions;
+
+namespace Zadatak1
+{
+    public class TodoSqlRepository : ITodoRepository
+    {
+
+        private readonly TodoDbContext _context;
+
+        public TodoSqlRepository(TodoDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TodoItem todoItem)
+        {
+            if (_context.Items.Find(todoItem.Id) != null)
+            {
+                throw new DuplicateTodoItemException($"duplicate id: {todoItem.Id}");
+            }
+            _context.Items.Add(todoItem);
+            _context.SaveChanges();
+        }
+
+        public async Task AddAsync(TodoItem todoItem)
+        {
+            if (await _context.Items.FirstOrDefaultAsync(t => t.Id == todoItem.Id) != null)
+            {
+                throw new DuplicateTodoItemException($"duplicate id: {todoItem.Id}");
+            }
+            _context.Items.Add(todoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public TodoItem Get(Guid todoId, Guid userId)
+        {
+            TodoItem item = _context.Items.Find(todoId);
+            if (item == null)
+            {
+                return null;
+            }
+            if (item.UserId != userId)
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+            return item;
+        }
+
+        public async Task<TodoItem> GetAsync(Guid todoId, Guid userId)
+        {
+            TodoItem todo = await _context.Items.FirstOrDefaultAsync(t => t.Id == todoId);
+            if (todo == null)
+            {
+                return null;
+            }
+            if (todo.UserId != userId)
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+            return todo;
+        }
+
+        public List<TodoItem> GetActive(Guid userId)
+        {
+            return _context.Items.Where(i => !i.DateCompleted.HasValue && i.UserId == userId).ToList();
+        }
+
+        public async Task<List<TodoItem>> GetActiveAsync(Guid userId)
+        {
+            return await _context.Items.Where(i => !i.DateCompleted.HasValue && i.UserId==userId).ToListAsync();
+        }
+
+        public List<TodoItem> GetAll(Guid userId)
+        {
+            return _context.Items.Where(i => i.UserId == userId).OrderByDescending(i => i.DateCreated).ToList();
+        }
+
+        public async Task<List<TodoItem>> GetAllAsync(Guid userId)
+        {
+            return await _context.Items.Where(i => i.UserId == userId).OrderByDescending(i => i.DateCreated).ToListAsync();
+        }
+
+        public List<TodoItem> GetCompleted(Guid userId)
+        {
+            return _context.Items.Where(i => i.DateCompleted.HasValue && i.UserId == userId).ToList();
+        }
+
+        public async Task<List<TodoItem>> GetCompletedAsync(Guid userId)
+        {
+            return await _context.Items.Where(i => i.DateCompleted.HasValue && i.UserId == userId).ToListAsync();
+        }
+
+        public List<TodoItem> GetFiltered(Func<TodoItem, bool> filterFunction, Guid userId)
+        {
+            return _context.Items.Where(filterFunction).Where(i => i.UserId == userId).ToList();
+        }
+
+        public async Task<List<TodoItem>> GetFilteredAsync(Expression<Func<TodoItem, bool>> filterFunction, Guid userId)
+        {
+            return await _context.Items.Where(filterFunction).Where(i => i.UserId == userId).ToListAsync();
+        }
+
+        public bool MarkAsCompleted(Guid todoId, Guid userId)
+        {
+            TodoItem item = Get(todoId, userId);
+            if (item != null)
+            {
+                item.MarkAsCompleted();
+                Update(item, item.UserId);
+                return true;
+            }
+            else
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+        }
+
+        public async Task<bool> MarkAsCompletedAsync(Guid todoId, Guid userId)
+        {
+            TodoItem item = await GetAsync(todoId, userId);
+           if(item != null)
+            {
+                item.MarkAsCompleted();
+                await UpdateAsync(item, item.UserId);
+                return true;
+            }
+            else
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+        }
+
+        public bool Remove(Guid todoId, Guid userId)
+        {
+            TodoItem item = _context.Items.Find(todoId);
+            if (item == null)
+                return false;
+            if (item.UserId != userId)
+            {
+                throw new TodoAccessDeniedException("Access denied. User must own this item to access it.");
+            }
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public async Task<bool> RemoveAsync(Guid todoId, Guid userId)
+        {
+            TodoItem item = await _context.Items.FirstOrDefaultAsync(t => t.Id == todoId);
+            if (item == null)
+                return false;
+            if (item.UserId != userId)
+            {
+                throw new TodoAccessDeniedException("Access denied. User must own this item to access it.");
+            }
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public void Update(TodoItem todoItem, Guid userId)
+        {
+            if (Get(todoItem.Id, userId) == null)
+            {
+                Add(todoItem);
+            }
+            else if (todoItem.UserId == userId)
+            {
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+        }
+
+        public async Task UpdateAsync(TodoItem todoItem, Guid userId)
+        {
+            if (await GetAsync(todoItem.Id, userId) == null)
+            {
+                await AddAsync(todoItem);
+            }
+            else if(todoItem.UserId==userId)
+            {
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new TodoAccessDeniedException("Access denied.");
+            }
+        }
+
+        public async Task<List<TodoItemLabel>> GetLabelsAsync(List<string> names)
+        {
+            return await _context.Labels.Where(l => names.Contains(l.Value)).ToListAsync();
+        }
+
+    }
+}
---
+++ Zadatak2/Controllers/TodoController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Zadatak1;
+using Microsoft.AspNetCore.Identity;
+using Zadatak2.Models;
+using Zadatak2.Models.TodoViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Zadatak2.Controllers
+{
+    [Authorize]
+    public class TodoController : Controller
+    {
+
+        private readonly ITodoRepository _todoRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TodoController(ITodoRepository todoRepository, UserManager<ApplicationUser> signInManager)
+        {
+            _todoRepository = todoRepository;
+            _userManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            var activeItems = await _todoRepository.GetActiveAsync(new Guid(user.Id));
+
+            IndexViewModel model = new IndexViewModel();
+
+            foreach(TodoItem item in activeItems)
+            {
+                model.TodoViewModels.Add(new TodoViewModel(item));
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTodoViewModel todoModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+                if (user == null)
+                {
+                    return Forbid("User not found.");
+                }
+                List<string> labels = todoModel.Labels
+                    .Split(',')
+                    .Select(l => l.Trim())
+                    .Where(l => !string
+                    .IsNullOrWhiteSpace(l))
+                    .Distinct()
+                    .ToList();
+
+                List<TodoItemLabel> labelList = await _todoRepository.GetLabelsAsync(labels);
+                labels.Except(labelList.Select(l => l.Value)).ToList().ForEach(l2 =>
+                {
+                    labelList.Add(new TodoItemLabel(l2));
+                });
+                TodoItem todoItem = new TodoItem(todoModel.Text, Guid.NewGuid())
+                {
+                    UserId = new Guid(user.Id),
+                    DateDue = todoModel.DateDue,
+                    Labels = labelList
+                };
+                await _todoRepository.AddAsync(todoItem);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todoModel);
+        }
+
+        public async Task<IActionResult> GetCompleted()
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            var completedItems = await _todoRepository.GetCompletedAsync(new Guid(user.Id));
+
+            CompletedViewModel model = new CompletedViewModel();
+
+            foreach (TodoItem item in completedItems)
+            {
+                model.TodoViewModels.Add(new TodoViewModel(item));
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> MarkAsCompleted(Guid id)
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            await _todoRepository.MarkAsCompletedAsync(id, new Guid(user.Id));
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveFromCompleted(Guid id)
+        {
+            ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
+
+            await _todoRepository.RemoveAsync(id, new Guid(user.Id));
+
+            return RedirectToAction(nameof(GetCompleted));
+        }
+    }
+}
